4860fd39e5e7a5976e45b11d26f5bdaf18ae9324
kai <kai@ezorderly.com>
2022-01-11 20:14:33 +0800

    draft

---
+++ js_sdk_example.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i['urDmpObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
+})(window,document,'script','https://orderly-public.s3.ap-southeast-1.amazonaws.com/sdk/universal_tw.js','cdp');
+
+var uid = gon.uid || ''
+cdp('create','00000000000000000000000000000000',uid);
+cdp('send','pageview');
+cdp('get','audiences');
+</script>
